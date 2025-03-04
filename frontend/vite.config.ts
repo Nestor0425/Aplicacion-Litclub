9179bb2e4e64e78940e3925865ba1796aaee494c
Alexisrdz1219 <ealexisrdz19@gmail.com>
2025-03-03 21:36:34 -0600

    Ajustes en vite.config.ts y solución de chunkSizeWarningLimit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   build:{
+//     rollupOptions:{
+//       output:{
+//         manualChunks:{
+//           pdfjs: ['pdfjs_dist'],
+//         }
+//       }
+//     }
+//   },
+//   chunkSizeWarningLimit: 1000,
+//   plugins: [react()],
+// });
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Aumenta el límite de advertencia de tamaño de chunk
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
