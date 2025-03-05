d0983ce9c62789968f206db096d5b1e081073b2d
Alexisrdz1219 <ealexisrdz19@gmail.com>
2025-03-04 19:18:12 -0600

    Conexión a PostgreSQL en Render

--- backend/dist/config/db.js
+++ backend/dist/config/db.js
@@ -8,6 +8,6 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const pool = new pg_1.Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+    ssl: { rejectUnauthorized: false }  // ✅ Para Render
 });
 exports.default = pool;
