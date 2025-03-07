d3c7a69eedf5da9eb3c3eb3f9bdb0429c10fd9df
Alexisrdz1219 <ealexisrdz19@gmail.com>
2025-03-06 19:33:00 -0600

    06

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 import { register } from "../controllers/user.controller";
 
 const router = Router();
-router.post("/register", register);
+router.post("/register", register, (req, res) => {
+    res.json({ message: "Ruta de registro funcionando" });
+  });
+  
 
 export default router;
